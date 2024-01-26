242890be69397b51c92c364b63bdfef0d550e014
Devon Wagstaff <devwags@gmail.com>
2024-01-26 11:49:04 -0700

    adds jest and prettier. modifies gitignore.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
-import { AppBar, Box, IconButton } from '@mui/material'
-import { AccountCircle } from '@mui/icons-material'
-import './App.css';
+import { AppBar, Box, IconButton } from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
+import "./App.css";
 
 const loggedIn = false;
 
 function App() {
   return (
-    <AppBar position='static' sx={{flexgrow: 1, height:"3em"}}>
-      <Box display="flex" flexDirection="row-reverse" sx={{height:"100%"}}>
-        {loggedIn ? <AccountCircle sx={{height:"100%", pr: 1}}/> : <IconButton color='inherit'>Login</IconButton>}
+    <AppBar position="static" sx={{ flexgrow: 1, height: "3em" }}>
+      <Box display="flex" flexDirection="row-reverse" sx={{ height: "100%" }}>
+        {loggedIn ? (
+          <AccountCircle sx={{ height: "100%", pr: 1 }} />
+        ) : (
+          <IconButton color="inherit">Login</IconButton>
+        )}
       </Box>
     </AppBar>
   );
