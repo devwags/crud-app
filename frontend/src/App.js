31247cfc5286ba16285dbefe3ba20cdddc383f1a
Devon Wagstaff <devwags@gmail.com>
2024-01-26 12:35:58 -0700

    extracts the AppBar component and functionality to a Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,8 @@
-import { AppBar, Box, IconButton } from "@mui/material";
-import { AccountCircle } from "@mui/icons-material";
 import "./App.css";
-
-const loggedIn = false;
+import Header from "./components/Header/Header";
 
 function App() {
-  return (
-    <AppBar position="static" sx={{ flexgrow: 1, height: "3em" }}>
-      <Box display="flex" flexDirection="row-reverse" sx={{ height: "100%" }}>
-        {loggedIn ? (
-          <AccountCircle sx={{ height: "100%", pr: 1 }} />
-        ) : (
-          <IconButton color="inherit">Login</IconButton>
-        )}
-      </Box>
-    </AppBar>
-  );
+  return <Header />;
 }
 
 export default App;
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,47 @@
+import { AppBar, Box, Link } from "@mui/material";
+import { AccountCircle, Home } from "@mui/icons-material";
+
+const loggedIn = true;
+
+const Header = () => {
+  return (
+    <AppBar position="static" sx={{ flexgrow: 1, height: "3em" }}>
+      <Box
+        display="flex"
+        flexDirection="row-reverse"
+        alignItems="center"
+        gap="2em"
+        sx={{ height: "100%" }}
+      >
+        {loggedIn ? (
+          <>
+            <AccountCircle sx={{ height: "100%", pr: "1em" }} />
+            <Link
+              href="#"
+              fontWeight="bold"
+              underline="hover"
+              color="inherit"
+              variant="body1"
+            >
+              My Inventory
+            </Link>
+          </>
+        ) : (
+          <Link
+            href="#"
+            fontWeight="bold"
+            underline="hover"
+            color="inherit"
+            variant="body1"
+            pr="1em"
+          >
+            Login
+          </Link>
+        )}
+        <Home sx={{ marginRight: "auto", pl: "1em" }} />
+      </Box>
+    </AppBar>
+  );
+};
+
+export default Header;
